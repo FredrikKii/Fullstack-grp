645c8f2f2c7cf86211d84b263a83fbc9db110920
FredrikKii <f.eriksson@mail.com>
2024-09-24 09:44:41 +0200

    first commit

---
+++ backend-src/server.ts
@@ -0,0 +1,23 @@
+// import
+// konfigurera server
+import express, { Express, NextFunction, Request, Response } from 'express'
+import { router as cookieRouter } from './routes/cookies.js'
+const app: Express = express()
+const port = 1338
+
+// middleware
+// route handlers
+app.use('/', (req: Request, res: Response, next: NextFunction) => {
+	// TODO: add body middleware and print it
+	console.log(`${req.method}  ${req.url}`, req.body)
+	next()
+})
+
+app.use('/cookies', cookieRouter)
+
+
+// starta servern
+app.listen(port, () => {
+	console.log('SERVER is listening on port ' + port)
+})
+
