496b7750b2b4bde2d9a5dd640cad61645107f055
Johan Mattsson <mattsson.johan.93@gmail.com>
2024-09-26 14:53:54 +0200

    user/hats fixes

--- backend-src/database/users.ts
+++ backend-src/database/users.ts
@@ -1,73 +1,55 @@
-import { MongoClient, Db, Collection, WithId, ObjectId } from "mongodb";
+import { MongoClient, Db, Collection, WithId, ObjectId, InsertOneResult, DeleteResult } from "mongodb";
 import { User } from "../models/user.js";
-import dotenv from 'dotenv';
-
-
-// Ladda miljövariabler från .env-filen
+// Vet inte varför, men var tvungen att ha detta för att det skulle fungera.
+import dotenv from "dotenv";
 dotenv.config();
 
-// Hämta connectionstringen
 const con: string | undefined = process.env.CONNECTION_STRING;
-const dbName = "webshop"; 
-const client = new MongoClient(con || "");
-let currentId = 1; // För att hantera ID-inkrementering
+// const test: string | undefined = process.env.TEST;
 
-async function getDb(): Promise<Db> {
+
+async function connectToDatabaseFindUsers(): Promise<Collection<User>> {
     if (!con) {
         console.log("No connection string, check your .env file!");
         throw new Error("No connection string");
     }
-    await client.connect();
-    return client.db(dbName);
+
+    const client: MongoClient = await MongoClient.connect(con);
+    const db: Db = await client.db("webshop");
+    return db.collection<User>("user"); 
 }
 
-// Hämta ALLA users
-export async function getAllUsers(): Promise<WithId<User>[]> {
-    const db = await getDb();
-    const usersCollection: Collection<User> = db.collection("user");
-    return await usersCollection.find().toArray();
-
+// Hitta alla users
+async function getAllUsers(): Promise<WithId<User>[]> {
+    const col = await connectToDatabaseFindUsers(); 
+    const result: WithId<User>[] = await col.find({}).toArray();
+    return result;
 }
 
-// Hämta EN specifik user
-export async function getUserById(id: ObjectId): Promise<WithId<User> | null> {
-    const db = await getDb();
-    const usersCollection: Collection<User> = db.collection("user");
-    return await usersCollection.findOne({ _id: id });
-
-	// async function getHatById(id: ObjectId): Promise<WithId<Hat> | null> {
-	// 	const col = await connectToDatabaseFindHats();
-	// 	const hat = await col.findOne({ _id: id }); // Use ObjectId directly in the query
-	// 	return hat;
-	// }
+// Hitta en user
+async function getOneUser(id: string): Promise<WithId<User> | null> {
+    const col = await connectToDatabaseFindUsers(); 
+    const result: WithId<User> | null = await col.findOne({ _id: new ObjectId(id) });
+    return result;
 }
 
-// Skapa ny user
-export async function createUser(user: User): Promise<User> {
-    const db = await getDb();
-    const usersCollection: Collection<User> = db.collection("user");
-    user._id = currentId++; // Tilldela nytt ID
-    await usersCollection.insertOne(user);
-    return user; // Returera användaren med det tilldelade ID:t
+// Lägg till user
+async function insertOneUser(user: User): Promise<ObjectId | null> {
+    const col = await connectToDatabaseFindUsers(); 
+    const result: InsertOneResult<User> = await col.insertOne(user);
+    if (!result.acknowledged) {
+        console.log("Could not insert user!");
+        return null;
+    }
+    return result.insertedId;
 }
 
-// Uppdatera user
-export async function updateUser(userId: number, user: User): Promise<any> {
-    const db = await getDb();
-    const usersCollection: Collection<User> = db.collection("user");
-    return await usersCollection.updateOne({ _id: userId }, { $set: user });
+// Ta bort en user
+async function deleteOneUser(userId: ObjectId): Promise<ObjectId | null> {
+    const col = await connectToDatabaseFindUsers(); 
+    const result: DeleteResult = await col.deleteOne({ _id: userId }); 
+
+    return userId; 
 }
 
-// Ta bort user
-export async function deleteUser(userId: number): Promise<any> {
-    const db = await getDb();
-    const usersCollection: Collection<User> = db.collection("user");
-    return await usersCollection.deleteOne({ _id: userId });
-}
-
-// Sök efter user
-export async function searchUsers(query: string): Promise<User[]> {
-    const db = await getDb();
-    const usersCollection: Collection<User> = db.collection("user");
-    return await usersCollection.find({ name: { $regex: query, $options: 'i' } }).toArray();
-}
+export { connectToDatabaseFindUsers, getAllUsers, getOneUser, insertOneUser, deleteOneUser};
--- backend-src/models/user.ts
+++ backend-src/models/user.ts
@@ -1,5 +1,7 @@
+import { ObjectId } from "mongodb";
+
 export interface User {
-    _id: number;  
+    _id: ObjectId;
     name: string;
     isAdmin: boolean;
 }
