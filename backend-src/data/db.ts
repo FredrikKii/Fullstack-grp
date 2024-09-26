2d76d730ad9ba8d9afe2e8b1f555251f57870e21
FredrikKii <f.eriksson@mail.com>
2024-09-26 09:21:02 +0200

    connection fix

--- backend-src/data/db.ts
+++ backend-src/data/db.ts
@@ -4,7 +4,7 @@ let db: Db;
 
 export const connectDB = async (): Promise<Db> => {
     if (db) return db; 
-    const client = new MongoClient('your_mongodb_connection_string');
+    const client = new MongoClient('mongodb+srv://fredrikkii:Freddan1991@cluster0.q07xi.mongodb.net/');
     await client.connect();
     db = client.db('webshop'); 
     console.log('Database connected successfully');
