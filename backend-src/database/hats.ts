88f36998295a56b47244bb6bb0367acbb0403a75
Anton Abrahamsson <abrahamsson37@gmail.com>
2024-09-27 11:07:19 +0200

    update hat

--- backend-src/database/hats.ts
+++ backend-src/database/hats.ts
@@ -1,4 +1,13 @@
-import { MongoClient, Db, Collection, WithId, ObjectId, InsertOneResult, DeleteResult } from "mongodb";
+import {
+    MongoClient,
+    Db,
+    Collection,
+    WithId,
+    ObjectId,
+    InsertOneResult,
+    DeleteResult,
+    UpdateResult,
+} from "mongodb";
 import { Hat } from "../models/hats.js";
 // Vet inte varför, men var tvungen att ha detta för att det skulle fungera.
 import dotenv from "dotenv";
@@ -7,35 +16,36 @@ dotenv.config();
 const con: string | undefined = process.env.CONNECTION_STRING;
 // const test: string | undefined = process.env.TEST;
 
-
 async function connectToDatabaseFindHats(): Promise<Collection<Hat>> {
     if (!con) {
         console.log("No connection string, check your .env file!");
         throw new Error("No connection string");
     }
-    
+
     const client: MongoClient = await MongoClient.connect(con);
     const db: Db = await client.db("webshop");
-    return db.collection<Hat>("hats"); 
+    return db.collection<Hat>("hats");
 }
 
 // Hitta alla hattar
 async function getAllHats(): Promise<WithId<Hat>[]> {
-    const col = await connectToDatabaseFindHats(); 
+    const col = await connectToDatabaseFindHats();
     const result: WithId<Hat>[] = await col.find({}).toArray();
     return result;
 }
 
 // Hitta en hatt
 async function getOneHat(id: string): Promise<WithId<Hat> | null> {
-    const col = await connectToDatabaseFindHats(); 
-    const result: WithId<Hat> | null = await col.findOne({ _id: new ObjectId(id) });
+    const col = await connectToDatabaseFindHats();
+    const result: WithId<Hat> | null = await col.findOne({
+        _id: new ObjectId(id),
+    });
     return result;
 }
 
 // Lägg till hatt
 async function insertOneHat(hat: Hat): Promise<ObjectId | null> {
-    const col = await connectToDatabaseFindHats(); 
+    const col = await connectToDatabaseFindHats();
     const result: InsertOneResult<Hat> = await col.insertOne(hat);
     if (!result.acknowledged) {
         console.log("Could not insert hat!");
@@ -46,12 +56,27 @@ async function insertOneHat(hat: Hat): Promise<ObjectId | null> {
 
 // Ta bort en hatt
 async function deleteOneHat(hatId: ObjectId): Promise<ObjectId | null> {
-    const col = await connectToDatabaseFindHats(); 
-    const result: DeleteResult = await col.deleteOne({ _id: hatId }); 
-
-    return hatId; 
+    const col = await connectToDatabaseFindHats();
+    const result: DeleteResult = await col.deleteOne({ _id: hatId });
+    return hatId;
 }
 
-export { connectToDatabaseFindHats, getAllHats, getOneHat, insertOneHat, deleteOneHat
+//  Uppdaterar en befintlig keps.. vrf har jag skrivit hatt överallt?
+async function updateOneHat(id: string, updatedHat: Hat): Promise<any> {
+    const col = await connectToDatabaseFindHats();
+    const result: UpdateResult<Hat> = await col.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: updatedHat }
+    );
 
- };
+    return result;
+}
+
+export {
+    connectToDatabaseFindHats,
+    getAllHats,
+    getOneHat,
+    insertOneHat,
+    deleteOneHat,
+    updateOneHat,
+};
--- backend-src/routes/hats.ts
+++ backend-src/routes/hats.ts
@@ -1,6 +1,12 @@
 import express, { Request, Response, Router } from "express";
 import { Hat } from "../models/hats.js";
-import { getAllHats, getOneHat, insertOneHat, deleteOneHat } from "../database/hats.js";
+import {
+    getAllHats,
+    getOneHat,
+    insertOneHat,
+    deleteOneHat,
+    updateOneHat,
+} from "../database/hats.js";
 import { ObjectId, WithId } from "mongodb";
 
 export const router: Router = express.Router();
@@ -13,32 +19,32 @@ router.get("/", async (req: Request, res: Response<WithId<Hat>[]>) => {
 
 // Hämtar ut en hatt, använd hattnamnet .
 router.get("/:id", async (req: Request, res: Response<WithId<Hat> | null>) => {
-    const id: string = req.params.id;  
+    const id: string = req.params.id;
     console.log("ID is: " + id);
     try {
-        const hat = await getOneHat(id);  
+        const hat = await getOneHat(id);
         if (hat) {
-            res.send(hat); 
+            res.send(hat);
         } else {
-            res.sendStatus(404);  
+            res.sendStatus(404);
         }
     } catch (error) {
-        console.error(error); 
-        res.sendStatus(500);  
+        console.error(error);
+        res.sendStatus(500);
     }
 });
 
 // Lägger till hatt
 router.post("/", async (req: Request, res: Response) => {
-    const newHat: Hat = req.body;     
+    const newHat: Hat = req.body;
     console.log("New hat data received:", newHat);
 
     try {
-        const insertedId = await insertOneHat(newHat); 
+        const insertedId = await insertOneHat(newHat);
         if (insertedId) {
-            res.status(201).send({ id: insertedId }); 
+            res.status(201).send({ id: insertedId });
         } else {
-            res.sendStatus(400);  
+            res.sendStatus(400);
         }
     } catch (error) {
         console.error("Error inserrting:", error);
@@ -48,23 +54,46 @@ router.post("/", async (req: Request, res: Response) => {
 
 // Tar bort hatt
 router.delete("/:id", async (req: Request, res: Response) => {
-    const hatId: string = req.params.id; 
+    const hatId: string = req.params.id;
     console.log("Request to delete hat with ID:", hatId);
 
     try {
         if (!ObjectId.isValid(hatId)) {
-            return res.sendStatus(400); 
+            return res.sendStatus(400);
         }
 
-        const deletedId = await deleteOneHat(new ObjectId(hatId)); 
+        const deletedId = await deleteOneHat(new ObjectId(hatId));
 
         if (deletedId) {
-            res.sendStatus(204); 
+            res.sendStatus(204);
         } else {
-            res.sendStatus(404); 
+            res.sendStatus(404);
         }
     } catch (error) {
         console.error("Error deleting hat:", error);
-        res.sendStatus(500); 
+        res.sendStatus(500);
+    }
+});
+
+// Uppdaterar en befintlig keps
+router.put("/:id", async (req: Request, res: Response) => {
+    // Detta är vad användaren matar in i body:
+    const updatedHat: Hat = req.body;
+
+    // Detta är vilken användaren vill ändra
+    const id: string = req.params.id;
+
+    console.log("Updated hat data received:", updatedHat);
+
+    try {
+        const insertedId = await updateOneHat(id, updatedHat);
+        if (insertedId) {
+            res.status(201).send({ id: insertedId });
+        } else {
+            res.sendStatus(400);
+        }
+    } catch (error) {
+        console.error("Error inserrting:", error);
+        res.sendStatus(500);
     }
 });
