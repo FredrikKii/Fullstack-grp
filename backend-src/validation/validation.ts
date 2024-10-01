f5446da735a6636dbd5a422383fa2885f4eaf67b
Anton Abrahamsson <abrahamsson37@gmail.com>
2024-10-01 11:30:22 +0200

    merge validation with hats

--- backend-src/validation/validation.ts
+++ backend-src/validation/validation.ts
@@ -1,18 +1,16 @@
-
 import Joi from "joi";
 import { User } from "../models/user-model.js";
 
 export const userSchema = Joi.object({
-    name: Joi.string().min(1).max(255).required(), 
+    name: Joi.string().min(1).max(255).required(),
     isAdmin: Joi.boolean().truthy("true").falsy("false").required(),
 });
 
 export const hatSchema = Joi.object({
-    name: Joi.string().min(1).max(255).required(),         
-    price: Joi.number().positive().precision(4).required(), 
+    name: Joi.string().min(1).max(255).required(),
+    price: Joi.number().positive().precision(4).required(),
     amountInStock: Joi.number().integer().min(0).required(),
-	image: Joi.string().optional()
-	 
+    image: Joi.string().optional(),
 });
 
 export const validateUser = (user: any) => {
@@ -20,13 +18,11 @@ export const validateUser = (user: any) => {
 };
 
 export const validateHat = (user: any) => {
-    return hatSchema.validate(user); 
+    return hatSchema.validate(user);
 };
-
 
 export const validateSearchQuery = (query: any) => {
-    return Joi.string().min(1).max(255).required().validate(query); 
+    return Joi.string().min(1).max(255).required().validate(query);
 };
-
 
 //.truthy("true").falsy("false")
